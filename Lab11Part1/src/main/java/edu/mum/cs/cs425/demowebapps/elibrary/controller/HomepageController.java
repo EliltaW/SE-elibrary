47f725a3b45412210e746c0c9fc853b6c2a944f7
EliltaW <eliltawondimu12@gmail.com>
2022-01-28 08:55:23 -0600

    elibrarys homepage

---
+++ Lab11Part1/src/main/java/edu/mum/cs/cs425/demowebapps/elibrary/controller/HomepageController.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.cs425.demowebapps.elibrary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomepageController {
+
+    @GetMapping(value = {"/", "/elibrary", "/elibrary/home"})
+    public String displayHomePage(){
+        return "home/index";
+    }
+}
